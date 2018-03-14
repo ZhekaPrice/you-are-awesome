8c6617e50373545aa976770fa1cc8c00272ebbb2
ZhekaPrice <36643027+ZhekaPrice@users.noreply.github.com>
2018-03-14 21:03:03 +0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,18 +1,47 @@
 // DO WHATEVER YOU WANT HERE
 
-const createEnumerableProperty = () => {};
-const createNotEnumerableProperty = () => {};
-const createProtoMagicObject = () => {};
-const incrementor = () => {};
+const createEnumerableProperty = (property) => {property = 1;};
+const createNotEnumerableProperty = () => {
+
+};
+const createProtoMagicObject = () => {
+    let temp = () => {};
+    temp.prototype = temp.__proto__;
+    return temp;
+};
+const incrementor = (() => {
+    let counter = 0;
+    let ReturnFunc = function Lol() {
+        counter++;
+        return Lol;
+    };
+    ReturnFunc.valueOf = () => counter;
+    return ReturnFunc;
+})();
 const asyncIncrementor = () => {};
-const createIncrementer = () => {};
+const createIncrementer = (() => {
+    let counter = 0;
+    let ResultFunc = function* Lol() {};
+    ResultFunc.prototype.next = () => {
+        return {
+            value: ++counter,
+            done: true
+        };
+    };
+    return ResultFunc;
+})();
 
 // return same argument not earlier than in one second, and not later, than in two
 const returnBackInSecond = () => {};
 const getDeepPropertiesCount = () => {};
 const createSerializedObject = () => {};
 const toBuffer = () => {};
-const sortByProto = () => {};
+const sortByProto = (value) => {
+    value.sort((x,y) => {
+        return y - x;
+    });
+    return value;
+};
 
 exports.createEnumerableProperty = createEnumerableProperty;
 exports.createNotEnumerableProperty = createNotEnumerableProperty;
